9559e5501ef39c5fb5f0e6558361b1288a8fcbce
K.VARUN <191619509+SolFlare-V@users.noreply.github.com>
2025-12-16 15:46:55 +0530

    Time: 0 ms (100%), Space: 13.5 MB (31.76%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,9 +1,6 @@
 class Solution(object):
     def missingNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        for i in range(len(nums)+1):
-            if i not in nums:
-                return i
+        s = 0
+        for i in range(1,len(nums)+1):
+            s += i
+        return s - sum(nums)
