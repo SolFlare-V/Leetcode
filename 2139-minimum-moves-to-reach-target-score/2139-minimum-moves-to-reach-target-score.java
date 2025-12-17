dbd59283f1fb32c7a96ad79be462b8130ab47ee4
K.VARUN <191619509+SolFlare-V@users.noreply.github.com>
2025-12-17 19:49:40 +0530

    Time: 0 ms (100%), Space: 42.1 MB (44.6%) - LeetHub

---
+++ 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minMoves(int target, int maxDoubles) {
+        int count = 0;
+        while (target>1 && maxDoubles>0){
+            if (target%2==0){
+                target/=2;
+                maxDoubles-=1;}
+            else{
+                target-=1;}
+            count+=1;
+        }
+        return count+ (target-1);
+    }
+}
