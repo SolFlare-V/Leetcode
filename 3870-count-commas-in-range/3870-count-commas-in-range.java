849ccfececef8b160aaf82e92e7a71494eedeff1
K.VARUN <k.varun2407@gmail.com>
2026-09-09 11:30:19 +0530

    Time: 4 ms (16.86%), Space: 42 MB (99.56%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int countCommas(int n) {
+        int count = 0;
+        if (n<1000){
+            return count;
+        }
+        for (int i=1000;i<=n;i++){
+            count+=1;
+        }
+        return count;
+    }
+}
