c235aa614585e313eb598a632fdfae6d208afce4
K.VARUN <k.varun2407@gmail.com>
2026-09-11 09:16:27 +0530

    Time: 76 ms (73.38%), Space: 134.5 MB (30.03%) - LeetHub

--- 0307-range-sum-query-mutable/0307-range-sum-query-mutable.java
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.java
@@ -1,61 +1,42 @@
 class NumArray {
-    int[] tree;
+    int[] bit;
+    int[] nums;
     int n;
 
     public NumArray(int[] nums) {
+        this.nums = nums;
         n = nums.length;
-        tree = new int[4 * n];
-        build(nums, 0, 0, n - 1);
+        bit = new int[n + 1];
+
+        for (int i = 0; i < n; i++)
+            add(i + 1, nums[i]);
     }
 
-    void build(int[] nums, int node, int l, int r) {
-        if (l == r) {
-            tree[node] = nums[l];
-            return;
+    void add(int i, int val) {
+        while (i <= n) {
+            bit[i] += val;
+            i += i & -i;
+        }
+    }
+
+    int sum(int i) {
+        int s = 0;
+
+        while (i > 0) {
+            s += bit[i];
+            i -= i & -i;
         }
 
-        int mid = (l + r) / 2;
-
-        build(nums, 2 * node + 1, l, mid);
-        build(nums, 2 * node + 2, mid + 1, r);
-
-        tree[node] = tree[2 * node + 1] + tree[2 * node + 2];
+        return s;
     }
 
     public void update(int index, int val) {
-        update(0, 0, n - 1, index, val);
-    }
-
-    void update(int node, int l, int r, int index, int val) {
-        if (l == r) {
-            tree[node] = val;
-            return;
-        }
-
-        int mid = (l + r) / 2;
-
-        if (index <= mid)
-            update(2 * node + 1, l, mid, index, val);
-        else
-            update(2 * node + 2, mid + 1, r, index, val);
-
-        tree[node] = tree[2 * node + 1] + tree[2 * node + 2];
+        int diff = val - nums[index];
+        nums[index] = val;
+        add(index + 1, diff);
     }
 
     public int sumRange(int left, int right) {
-        return query(0, 0, n - 1, left, right);
-    }
-
-    int query(int node, int l, int r, int ql, int qr) {
-        if (qr < l || r < ql)
-            return 0;
-
-        if (ql <= l && r <= qr)
-            return tree[node];
-
-        int mid = (l + r) / 2;
-
-        return query(2 * node + 1, l, mid, ql, qr)
-             + query(2 * node + 2, mid + 1, r, ql, qr);
+        return sum(right + 1) - sum(left);
     }
 }
