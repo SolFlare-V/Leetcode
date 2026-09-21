81685fa0bef154a7ac5ce9ef16932abbf9903e89
K.VARUN <k.varun2407@gmail.com>
2026-09-21 23:22:58 +0530

    Time: 1 ms (100%), Space: 46.6 MB (26.07%) - LeetHub

---
+++ 0393-utf-8-validation/0393-utf-8-validation.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean validUtf8(int[] data) {
+        int remaining = 0;
+
+        for (int num : data) {
+            if (remaining == 0) {
+                if ((num >> 5) == 0b110) {
+                    remaining = 1;
+                } else if ((num >> 4) == 0b1110) {
+                    remaining = 2;
+                } else if ((num >> 3) == 0b11110) {
+                    remaining = 3;
+                } else if ((num >> 7) != 0) {
+                    return false;
+                }
+            } else {
+                if ((num >> 6) != 0b10) {
+                    return false;
+                }
+                remaining--;
+            }
+        }
+
+        return remaining == 0;
+    }
+}
